32ce6df2011804bbec94943ec927bb48459b88f4
17603 <782117417@qq.com>
2020-01-13 20:43:19 +0800

    修改composer文件

--- src/Hash.php
+++ src/Hash.php
@@ -9,7 +9,7 @@
 // | Author: yunwuxin <448901948@qq.com>
 // +----------------------------------------------------------------------
 
-namespace helper;
+namespace ruansuccess\helper;
 
 
 class Hash
